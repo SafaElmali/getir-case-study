f8456cee19b011c83a1fbecdeef8daa95d79e052
SafaElmali <tsafaelmali@gmail.com>
2021-02-09 11:03:03 +0300

    feat: add edit icons

--- src/components/TodoList/components/TodoItem.js
+++ src/components/TodoList/components/TodoItem.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { CheckIcon } from "@chakra-ui/icons";
-import { Box, Flex, Text, Checkbox } from "@chakra-ui/react";
-import { BsFillTrashFill } from "react-icons/bs";
+import { Box, Flex, Text, Checkbox, Input } from "@chakra-ui/react";
+import { BsFillTrashFill, BsPencilSquare, BsCheckBox } from "react-icons/bs";
 import "./TodoItem.css";
 
 const TodoItem = ({ todo }) => {
   const [isChecked, setIsChecked] = useState(todo.checked);
+  const [isEdit, setIsEdit] = useState(false);
+  const [inputValue, setInputValue] = useState(todo.content);
+  const editInput = useRef();
+
+  /* Get current input value from user */
+  const handleTodoEdit = () => {
+    setInputValue(editInput.current.value);
+  };
+
   return (
     <Box marginTop="20px" padding={5} borderRadius={15} backgroundColor="#fff">
       <Flex
@@ -13,26 +22,62 @@ const TodoItem = ({ todo }) => {
         flexDirection="row"
         alignItems="center"
       >
-        <Flex>
-          <Checkbox
-            isChecked={isChecked}
-            size="lg"
-            icon={<CheckIcon />}
-            colorScheme="purple"
-            mr={3}
-            onChange={(e) => setIsChecked(e.target.checked)}
-          />
-          <Text
-            cursor="pointer"
-            onClick={() => setIsChecked(!isChecked)}
-            className={isChecked ? "todo__text--checked" : null}
-          >
-            {todo.content}
-          </Text>
+        {!isEdit ? (
+          <>
+            <Flex flexGrow={1}>
+              <Checkbox
+                isChecked={isChecked}
+                size="lg"
+                icon={<CheckIcon />}
+                colorScheme="purple"
+                mr={3}
+                onChange={(e) => setIsChecked(e.target.checked)}
+              />
+              <Text
+                cursor="pointer"
+                onClick={() => setIsChecked(!isChecked)}
+                className={isChecked ? "todo__text--checked" : null}
+              >
+                {todo.content}
+              </Text>
+            </Flex>
+          </>
+        ) : (
+          <Flex alignItems="center" flexGrow={1}>
+            <Input
+              type="text"
+              placeholder="edit to-do..."
+              value={inputValue}
+              ref={editInput}
+              onChange={handleTodoEdit}
+            />
+            {}
+            <BsCheckBox
+              fill={"green"}
+              size={"2em"}
+              style={{ marginLeft: 15 }}
+              onClick={() => setIsEdit(!isEdit)}
+            />
+          </Flex>
+        )}
+        <Flex
+          flexDirection="row"
+          flexWrap="wrap"
+          ml={4}
+          className="todo__action-container"
+          flexGrow={0.2}
+          justifyContent="flex-end"
+        >
+          {!isEdit ? (
+            <BsPencilSquare
+              fill={"#3182ce"}
+              size={"1.5em"}
+              style={{ marginRight: 15 }}
+              onClick={() => setIsEdit(!isEdit)}
+            />
+          ) : null}
+          <BsFillTrashFill fill={"#e53e3e"} size={"1.5em"} />
         </Flex>
-        <Box ml={4} className="trash__container">
-          <BsFillTrashFill fill={"#e01010"} size={"1.5em"} />
-        </Box>
       </Flex>
     </Box>
   );
